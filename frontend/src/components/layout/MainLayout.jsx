08ebe51b6a07f75a517e62dbebec26d787a7a585
Solomon Shmels <solomon@example.com>
2025-12-10 16:55:33 +0300

    Hide Footer on Quiz and Auth pages

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 
 
 function MainLayout() {
-
+  const location = useLocation();
+  const hideFooterRoutes = ["/quiz", "/signin", "/signup"];
+  const shouldHideFooter = hideFooterRoutes.includes(location.pathname);
 
   return (
     <>
@@ -12,7 +14,7 @@ function MainLayout() {
       <main className="pt-20 min-h-screen bg-[var(--color-bg)] text-[var(--color-text)] transition-[background,color] duration-300">
         <Outlet />
       </main>
-      <Footer />
+      {!shouldHideFooter && <Footer />}
     </>
   );
 }
